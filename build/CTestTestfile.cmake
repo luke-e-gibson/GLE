60b0ed90cfaf183052e9842049e8798b66e733f7
luke-e-gibson <luke.e.gibson@outlook.com>
2023-12-02 10:41:57 +1100

    Remove the now ignored directory "build"

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/lukeg/GLE
-# Build directory: /home/lukeg/GLE/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
